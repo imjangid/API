af1e285e8f95b2ade99bd0ce0d9eac557e32c5a8
Ashvini Kumar Jangid <ashvinijangid@gmail.com>
2018-03-23 18:06:16 +0530

    homePage Added

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    url(r"^$", include("home.urls")),
     path('admin/', admin.site.urls),
     url(r'^ocr/', include("ocr.urls")),
     url(r'^anagram/', include("anagram.urls")),
